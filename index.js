b12d1c9a085da7376e987a0fc65b3fb265afea70
knightamin <a.qorbani@gmail.com>
2024-03-26 20:30:55 +0330

    moralis api

---
+++ index.js
@@ -0,0 +1,22 @@
+const Moralis = require('moralis').default;
+const { SolNetwork } = require('@moralisweb3/common-sol-utils');
+
+const runApp = async () => {
+    await Moralis.start({
+        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImY1NzdjYWNkLThlYmEtNDYwNy1hZTk5LWU5YjFiNWZlYzI5MSIsIm9yZ0lkIjoiMzg0ODMyIiwidXNlcklkIjoiMzk1NDE0IiwidHlwZUlkIjoiMWNmODU0OTQtNWRlOC00NTE4LThhNzgtNmM0ZjY4ZTkzMzVjIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MTE0NjExMTgsImV4cCI6NDg2NzIyMTExOH0.-D6q6jpDQ-VHGbweG5ZPudUUnhX6dUgleIChAkbk4Wg',
+        // ...and any other configuration
+    });
+
+    const address = 'HhBXHoJCgMNyPRCx3qVEqHnUFNZpsUnfLDZBNK8gmvgh';
+
+    const network = SolNetwork.MAINNET;
+
+    const response = await Moralis.SolApi.account.getPortfolio({
+        address,
+        network,
+    });
+
+    console.log(response.toJSON());
+};
+
+runApp();
